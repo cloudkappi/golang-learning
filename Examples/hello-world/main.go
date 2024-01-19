c8d858711d519e95d989df50c71cf8cfca3cbce8
cloudkappi <cloudkappi@gmail.com>
2024-01-19 19:05:16 +0530

    golang-firstcode

---
+++ Examples/hello-world/main.go
@@ -0,0 +1,35 @@
+package	main
+
+import "fmt"
+
+// Example of Raw string literal
+
+func main() {
+	fmt.Println(`
+	Leaves of every hue ~ Sadhguru
+
+colourful feast for eye
+But a death dance
+Of colour. Not of gaiety
+but of cold gravity
+Crown and clothing
+all tumbling down
+Being stripped off
+of life making sap
+Trees will stand naked
+of leaf, flower or fruit
+Only in hope of
+another lease of life
+Autumnal fall
+Winter’s rot
+Spring’s surprise
+This not of life or death
+but of life and death
+Not a catastrophe
+But a carnival
+No where to go
+As all is Here.
+	`)
+}
+
+
---
+++ Examples/variables/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// DECLARE a Variable to hold value
+	// Define type of vaule to hold
+	// Assign the value
+	var h int = 10
+	fmt.Println(h)
+
+	//To assign empty value
+	var g int
+	fmt.Println(g)
+
+	// Short declaration operator better way to assign variables
+
+	a := 424
+	fmt.Println(a)
+
+	/*
+	    Zero values :
+		int 0
+		boolean false
+		float 0.0
+		string ""
+
+	*/
+
+}
